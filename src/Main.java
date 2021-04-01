08bd3f70e0c80bc2d1de83c776f5bba40a41e241
Dmitrii Nasedkin <2_tone@mail.ru>
2021-04-01 22:29:07 +0300

    all files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = balance + transfer;
+        System.out.println("Сумма на счете: " + total);
+    }
+}
+
